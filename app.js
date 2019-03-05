942f391a08890c8622d41acb66b82d4c23a3db94
Alimi Kehinde Marufudeen <alimi.kmaruf@gmail.com>
2019-03-05 01:11:37 +0100

    add style to input field and buttons

---
+++ app.js
@@ -0,0 +1,56 @@
+const button = document.querySelector('#enter');
+const input = document.querySelector('#userinput');
+const ul = document.querySelector('ul');
+const clearItem = document.querySelector('#clear');
+
+const checkInputLength = () => {
+  return input.value.length;
+}
+
+const createListElement = () => {
+  const li = document.createElement('li');
+  li.appendChild(document.createTextNode(input.value));
+  ul.appendChild(li);
+  input.value = '';
+
+  // toggles list item on click
+  const toggleListItemOnclick = () => {
+    li.classList.toggle('done');
+  }
+  li.addEventListener('click', toggleListItemOnclick);
+
+  // function to perform deletion
+  const deleteListItem = () => {
+    li.classList.add('delete');
+  }
+
+  // create delete buttons Icon next to each list item 
+  const delButton = document.createElement('i');
+  delButton.classList.add('fa', 'fa-trash');
+  li.appendChild(delButton);
+  delButton.addEventListener('click', deleteListItem);
+
+
+}
+
+// add list after button click
+const addListAfterClick = () => {
+  if (checkInputLength() > 0) {
+    createListElement();
+  }
+}
+// add list after enter btn key press
+const addListAfterKeypress = () => {
+  if (checkInputLength() > 0 && event.keyCode === 13) {
+    createListElement();
+  }
+}
+
+// function to clear list item after adding items
+const clearListItem = () => {
+  ul.innerHTML = '';
+}
+
+clearItem.addEventListener('click', clearListItem)
+button.addEventListener('click', addListAfterClick);
+input.addEventListener('keypress', addListAfterKeypress);
