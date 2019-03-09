4d46590a0f46cfe783a3e1826a23414016cf70d3
Alimi Kehinde Marufudeen <alimi.kmaruf@gmail.com>
2019-03-09 21:13:50 +0100

    add style for list item

--- app.js
+++ app.js
@@ -1,56 +1,64 @@
 const button = document.querySelector('#enter');
 const input = document.querySelector('#userinput');
-const ul = document.querySelector('ul');
+const ul = document.querySelector('.ordered-list');
 const clearItem = document.querySelector('#clear');
 
 const checkInputLength = () => {
-  return input.value.length;
+    return input.value.length;
 }
 
 const createListElement = () => {
-  const li = document.createElement('li');
-  li.appendChild(document.createTextNode(input.value));
-  ul.appendChild(li);
-  input.value = '';
+    const li = document.createElement('li');
+    li.appendChild(document.createTextNode(input.value));
+    ul.appendChild(li);
+    input.value = '';
 
-  // toggles list item on click
-  const toggleListItemOnclick = () => {
-    li.classList.toggle('done');
-  }
-  li.addEventListener('click', toggleListItemOnclick);
 
-  // function to perform deletion
-  const deleteListItem = () => {
-    li.classList.add('delete');
-  }
+    // add style to the created list-item
+    li.classList.add('list-creation');
 
-  // create delete buttons Icon next to each list item 
-  const delButton = document.createElement('i');
-  delButton.classList.add('fa', 'fa-trash');
-  li.appendChild(delButton);
-  delButton.addEventListener('click', deleteListItem);
+    // toggles list item on click
+    const toggleListItemOnclick = () => {
+        li.classList.toggle('done');
+    }
+    li.addEventListener('click', toggleListItemOnclick);
 
+    // function to perform deletion
+    const deleteListItem = () => {
+        li.classList.add('delete');
+
+    }
+
+    // create delete buttons Icon next to each list item 
+    const delButton = document.createElement('i');
+    delButton.classList.add('fa', 'fa-trash');
+    li.appendChild(delButton);
+    delButton.addEventListener('click', deleteListItem);
+    
+    // add style to delete button icon created
+    delButton.classList.add('del-button-icon')
 
 }
 
 // add list after button click
 const addListAfterClick = () => {
-  if (checkInputLength() > 0) {
-    createListElement();
-  }
+    if (checkInputLength() > 0) {
+        createListElement();
+    }
 }
-// add list after enter btn key press
+// add list after key press
 const addListAfterKeypress = () => {
-  if (checkInputLength() > 0 && event.keyCode === 13) {
-    createListElement();
-  }
+    if (checkInputLength() > 0 && event.keyCode === 13) {
+        createListElement();
+    }
 }
 
 // function to clear list item after adding items
 const clearListItem = () => {
-  ul.innerHTML = '';
-}
+    ul.innerHTML = '';
 
+}
+//event handlers
 clearItem.addEventListener('click', clearListItem)
 button.addEventListener('click', addListAfterClick);
 input.addEventListener('keypress', addListAfterKeypress);
